346fed3c9549d49abd17f72eb2fb7d449960e907
Sughosh Sudhanvan <bssughosh27@gmail.com>
2022-03-17 16:20:47 +0530

    chore: added required files for clean architecture

---
+++ lib/core/presentation/use_case_observer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+class UseCaseObserver implements Observer<void> {
+  final Function _onComplete;
+  final Function _onError;
+  final Function? onNextFunction;
+
+  UseCaseObserver(this._onComplete, this._onError, {this.onNextFunction});
+
+  @override
+  void onComplete() {
+    _onComplete();
+  }
+
+  @override
+  void onError(e) {
+    _onError(e);
+  }
+
+  @override
+  void onNext(value) {
+    onNextFunction!(value);
+  }
+}
